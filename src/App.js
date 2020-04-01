4338106c70b7f9446bcc3a9fb286c8a4de9dd590
Arunabh Arjun <arunabh@Arunabhs-MacBook-Pro.local>
2020-04-02 03:37:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './tofo.css';
+import './todo.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav } from 'react-bootstrap'
 
